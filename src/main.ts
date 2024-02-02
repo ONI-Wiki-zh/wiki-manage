4b1ee36d458d38c2eb0ff198574737e110939249
cnctemaR <42052819+cnctemaR@users.noreply.github.com>
2024-02-02 22:28:33 +0800

    add Nested Router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,27 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import routes from "virtual:generated-pages";
+import CodeDiff from "v-code-diff";
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+createApp(App)
+.use(router)
+.use(vuetify)
+.use(CodeDiff)
+.mount("#app");
