7a933217d2e963137ca3a0d25d3a185f5c4ec712
cnctemaR <42052819+cnctemaR@users.noreply.github.com>
2024-02-16 04:53:05 +0800

    update websocket

--- src/pages/index/StatisticPageStatus.vue
+++ src/pages/index/StatisticPageStatus.vue
@@ -18,7 +18,23 @@
     </v-row>
     <v-row class="bottom-row">
       <v-data-table :headers="headers" :items="pageItems" item-key="id" items-per-page="20" :fixed-header="true"
-        :fixed-footer="true" :sort-by="[]" multi-sort></v-data-table>
+        :fixed-footer="true" :sort-by="[]" multi-sort>
+        <template v-slot:item.latest_timestamp="{ item }">
+          <a :href="`${WikiSite.baseUrl}${item.title}`" target="_blank"><span>{{ DateFromat.formatDate(item.latest_timestamp) }}</span></a>
+        </template>
+        <template v-slot:item.outdated="{ item }">
+          <v-icon v-show="item.outdated" color="red">mdi-alert-circle</v-icon>
+        </template>
+        <template v-slot:item.noneTargetLangPage="{ item }">
+          <v-icon v-show="item.noneTargetLangPage" color="red">mdi-link-variant-off</v-icon>
+        </template>
+        <template v-slot:item.onewayLangLink="{ item }">
+          <v-icon v-show="item.onewayLangLink" color="red">mdi-link-variant-off</v-icon>
+        </template>
+        <template v-slot:item.multiBackLangLinks="{ item }">
+          <v-icon v-show="item.multiBackLangLinks" color="red">mdi-link-variant-off</v-icon>
+        </template>
+      </v-data-table>
     </v-row>
   </div>
 </template>
@@ -27,16 +43,17 @@
 import { ref, computed, onMounted, onUnmounted } from 'vue';
 import axios from 'axios';
 import { API, WS, WikiSite } from '@src/constant'
+import DateFromat from '@src/utils/DateFormat'
 import { fa } from 'vuetify/locale';
 
 export default {
   data: () => ({
     headers: [
-      { title: '名称', key: 'title', width: "15%" },
+      { title: '名称', key: 'title', width: "30%" },
       { title: '上次更新旧于目标站点', key: 'outdated' },
-      { title: '多语言链接引用页面不存在', key: 'non_existence' },
-      { title: '多语言链接单向引用', key: 'oneway' },
-      { title: '多语言链接反链不唯一', key: 'non_unique' },
+      { title: '多语言链接引用页面不存在', key: 'noneTargetLangPage' },
+      { title: '多语言链接单向引用', key: 'onewayLangLink' },
+      { title: '多语言链接反链不唯一', key: 'multiBackLangLinks' },
       { title: '上次编辑时间', key: 'latest_timestamp' },
     ],
   }),
@@ -46,6 +63,7 @@ export default {
     const _pageItems = ref([])
     const pageItems = computed(() => {
       let pages = _pageItems.value
+      pages = pages.filter(obj => { return obj.outdated == true || obj.noneTargetLangPage == true || obj.onewayLangLink == true || obj.multiBackLangLinks == true })
       if (!(itemFilter.value == "" || itemFilter.value == undefined)) {
         pages = pages.filter(obj => obj.title.toLowerCase().includes(itemFilter.value.toLowerCase()));
       }
@@ -60,47 +78,72 @@ export default {
     }
     function loadPagesData() {
       isQuerying.value = true
-      let reqUrl = API.pagestatus
+      let reqUrl = API.pagestatus + "?lang=en"
       axios.get(reqUrl)
         .then(res => {
           isQuerying.value = false
           if (res.status != 200 || res.headers['content-type'] != "application/json") {
             return
           }
-          _pageItems.value = res.data
+          if (res.data) {
+            _pageItems.value = res.data
+          }
         })
         .catch(error => {
           isQuerying.value = false
           console.log(error)
         });
     }
+    function sendPullRequest() {
+      if (!socket) {
+        return
+      }
+      console.log("WebSocket send")
+      let request = {
+        "lang": "en"
+      }
+      socket.send(JSON.stringify(request));
+    }
     function updatePagesData() {
       isQuerying.value = true
-      socket = new WebSocket(WS.pullPageStatus);
-      socket.addEventListener('open', (event) => {
-        console.log('WebSocket connected');
-        let request = {
-          "lang": "en"
-        }
-        socket.send(JSON.stringify(request));
-      });
-      socket.addEventListener('message', (event) => {
-        const response = JSON.parse(event.data);
-        _pageItems.value = response.data
-      });
+      if (!(socket && socket.readyState === WebSocket.OPEN)) {
+        socket = new WebSocket(WS.pullPageStatus);
+        socket.addEventListener('open', (event) => {
+          // 打开舞姿
+          console.log('WebSocket connected');
+          // 发起查询请求
+          sendPullRequest()
+        });
+        socket.addEventListener('close', (event) => {
+          // 关闭链接
+          console.log('WebSocket closed');
+        });
+        socket.addEventListener('message', (event) => {
+          // 接收消息
+          const response = JSON.parse(event.data);
+          console.log(response.code, response.data)
+          if (response.code == 100 && response.data) {
+            // 增量消息
+            _pageItems.value.push(...response.data)
+          } else if (response.code == 200 && response.data) {
+            // 全量消息
+            _pageItems.value = response.data
+            socket.close()
+            isQuerying.value = false
+          }
+        });
+      } else {
+        sendPullRequest()
+      }
     }
 
     onMounted(() => {
       loadPagesData()
     })
-    onUnmounted(()=>{
-      if(socket){
-        // 关闭
-      }
-    })
 
     return {
       WikiSite,
+      DateFromat,
       //Data
       isQuerying,
       pageItems,
